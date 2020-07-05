6516b5612b3cc12652837b8b5a9a03757e059087
zackzhangkai <zackzhang@yunify.com>
2020-07-05 16:51:41 +0800

    init

---
+++ initCentosDocker.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e 
+set -o 
+set -x 
+
+yum install -y bash-completion ebtables ipset tmux nfs-utils socat wget conntrack ceph-common glusterfs-client  && \
+
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine && \
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo && \
+
+yum install -y yum-utils docker-ce docker-ce-cli containerd.io && \
+
+systemctl start docker && systemctl enable docker
+
+

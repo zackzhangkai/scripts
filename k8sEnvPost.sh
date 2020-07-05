8467b1af0bfef117890b4640d61936e13f0a3eff
zackzhangkai <zackzhang@yunify.com>
2020-07-05 21:20:38 +0800

    support zsh bash

--- k8sEnvPost.sh
+++ k8sEnvPost.sh
@@ -6,17 +6,8 @@ set -x
 
 yum -y install bash-completion
 
-source <(kubectl completion bash)
-
-egrep -w "source <(kubectl completion bash)" /root/.bashrc ||  echo "source <(kubectl completion bash)" >> ~/.bashrc # 在您的 bash shell 中永久的添加自动补全
-
-alias k=kubectl
-egrep -w "alias k=kubectl" /root/.bashrc || echo "alias k=kubectl" >> /root/.bashrc
-
-complete -F __start_kubectl k
-egrep "complete -F __start_kubectl k" /root/.bashrc || echo "complete -F __start_kubectl k" >> /root/.bashrc
-
 # install krew
+ls /root/.krew || \
 (
   set -x; cd "$(mktemp -d)" &&
   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
@@ -25,19 +16,36 @@ egrep "complete -F __start_kubectl k" /root/.bashrc || echo "complete -F __start
   "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
   "$KREW" update
 )
+export PATH="${PATH}:/root/.krew/bin"
+egrep "/root/.krew/bin" /root/.bashrc || echo "export PATH="${PATH}:/root/.krew/bin"" >> /root/.bashrc
 
-kubectl krew install ns
+kubectl krew list | grep ns || kubectl krew install ns
 
 # install zsh
 if [ -n "$1" ]; then
     if [ "$1" == "zsh" ]; then
-	yum -y zsh
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	cd root/.oh-my-zsh/plugins/
-	git clone  https://github.com/zsh-users/zsh-autosuggestions.git
+	if [ ! -f /root/.zshrc ]; then
+	    yum -y install zsh
+	    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	fi
+        ls /root/.oh-my-zsh/plugins/zsh-autosuggestions &> /dev/null || ( cd /root/.oh-my-zsh/plugins/ && git clone https://github.com/zsh-users/zsh-autosuggestions.git )
 	sed -i 's/^ZSH_THEME=.*/ZSH_THEME="maran"/' /root/.zshrc
-	source <(kubectl completion zsh)  # 在 zsh 中设置当前 shell 的自动补全
-	erep -w "source <(kubectl completion zsh)" /root/.zshrc  || echo "source <(kubectl completion zsh)" >> /root/.zshrc
+	sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions)/' /root/.zshrc
+	egrep -w "source <(kubectl completion zsh)" /root/.zshrc  || echo "source <(kubectl completion zsh)" >> /root/.zshrc
     else
         echo "The option should be "zsh""
     fi
+  echo $SHELL | grep zsh || chsh -s /bin/zsh
+  echo "Do \"source /root/.zshrc\""
+
+else
+  egrep 'kubectl completion bash' /root/.bashrc ||  echo "source <(kubectl completion bash)" >> ~/.bashrc # 在您的 bash shell 中永久的添加自动补全
+  egrep -w "alias k=kubectl" /root/.bashrc || echo "alias k=kubectl" >> /root/.bashrc
+  egrep "complete -F __start_kubectl k" /root/.bashrc || echo "complete -F __start_kubectl k" >> /root/.bashrc
+  echo $SHELL | grep bash || chsh -s /bin/bash
+  echo "Do \"source /root/.bashrc\""
+
+fi
+
+
+
